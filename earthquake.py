c43d4a2747ca9e412599716ff84afc3068b21215
Dant3C <dcerlez1231@gmail.com>
2022-04-04 09:46:47 -0700

    added some python

--- earthquake.py
+++ earthquake.py
@@ -2,8 +2,7 @@ from flask import Flask, url_for, render_template, request, json
 
 with open('earthquakes.json') as json_earthquakes:
     earthquakes = json.load(json_earthquakes)
-    print (earthquakes[0]['location']['name'])
-    print (earthquakes[0]['location']['name'])
+
     
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 @app.route("/")
@@ -18,21 +17,31 @@ def get_earthquake_amount():
     # returns abount of earthquakes per countrie or us state
     earthquake_state = {}
     CA_earthquake = 0
+    state = ""
     for x in earthquakes:
         if x['location']['name'] in earthquake_state:
-            earthquake_state[(x['location'])]# finish looping through and adding missing spots
+            state = x['location']['name']
+            i = earthquake_state[state]
+            i += 1
+            earthquake_state[state] = i# finish looping through and adding missing spots
             # if california in earthquake_state add 1 to california
             # else add california to the dictornary and add one 
-        else
-        if x['location']['name'] == 'California':
-            CA_earthquake += 1
-    return CA_earthquake
-print (get_earthquake_amount())
-
-
-
-
-
+        else:
+            state = x['location']['name']
+            earthquake_state[state] = 1
+            
+    # return earthquake_state
+    quake_keys = earthquake_state.keys()
+    top_state = ""
+    a = 0
+    top_five = earthquake_state['Burma']
+    for x in earthquake_state:
+        if earthquake_state[x] > top_five:
+            top_five = earthquake_state[x]
+            top_state = quake_keys[a]
+            a += 1
+    return top_state
+print(get_earthquake_amount())
 
 
 if __name__=="__main__":
